d347eec99af4651c861d17150b4b0656a4c325ae
danielclough <danielclough@gmail.com>
2020-05-14 20:49:05 -0700

    update

--- src/pages/copyright.js
+++ src/pages/copyright.js
@@ -11,10 +11,10 @@ const CopyrightPage = () => (
       <main className="side-margin">
         <article className="post-content page-template no-image">
           <div className="post-content-body">
-            <h2 id="copyright">Photography Copyright</h2>
+            <h2 id="social-copyright">Social Sharing Copyright</h2>
             <h3 id="sharing">Sharing</h3>
             <p>
-              Images may be shared under conditions of the{" "}
+              Images published on Social Media are shared under conditions of the{" "}
               <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/">
                 Creative Commons 4.0 License.
               </a>{" "}
@@ -25,17 +25,37 @@ const CopyrightPage = () => (
               If you share my photography you are required by law to link to my
               webpage, Facebook page, or Instagram account.
             </p>
-            <h3 id="noncommercial">NonCommercial</h3>
+            <h3 id="non-commercial">Non-Commercial</h3>
             <p>
               You may not use my work for commercial purpose without my explicit
               written permission.
             </p>
-            <h3 id="noderivs">NoDerivs</h3>
+            <h3 id="no-derivs">No Derivs</h3>
             <p>
-              You may not edit my work without my explicit written permission. Do
-              not crop off the watermark.
+              You may not edit my work without my explicit written permission.  <br />
+              Do not crop off the watermark.
             </p>
-
+            <hr />
+          </div>
+          <div className="post-content-body">
+            <h2 id="commercial-copyright">Daniel's Commercial Photography - DCP 1.0</h2>
+            <h3 id="sharing">For Images Sold</h3>
+            <p>
+              Default Copyright
+              <br />
+            </p>
+            <h3 id="work-completed">For Work Completed</h3>
+            <p>
+              All obligation is complete upon payment unless otherwise noted in the invoice.
+            </p>
+            <h3 id="transformations">Transformations</h3>
+            <p>
+              Trasformations may be made only for promotion of those mentioned in the invoice. 
+            </p>
+            <h3 id="no-transfer">No Transfer of Copyright</h3>
+            <p>
+              No resale of images or trasfer of copyright.
+            </p>
             <hr />
           </div>
         </article> 
